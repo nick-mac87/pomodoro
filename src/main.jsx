95e3a1502333a92e6a5541cba4984fc07083eb70
Nicholas McCreath <nicholasmccreath@gmail.com>
2026-02-17 22:07:42 +0200

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import PixelPomo from './PixelPomo.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PixelPomo />
+  </React.StrictMode>,
+)
